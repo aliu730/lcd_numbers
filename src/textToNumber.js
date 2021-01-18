a14b3f32c4ee835af7bca41e0cb4cce3c4606ecf
Andrew Liu <aliu730@gmail.com>
2021-01-17 18:23:28 -0800

    refactor removing excess methods for layers

--- src/textToNumber.js
+++ src/textToNumber.js
@@ -6,14 +6,28 @@ const numberToLcd = (num) => {
     if (!num) {
         return "";
     }
+
+    let dict = {
+        "1": ["   ", "  |", "  |"],
+        "2": [" _ ", " _|", "|_ "],
+        "3": [" _ ", " _|", " _|"],
+        "4": ["   ", "|_|", "  |"],
+        "5": [" _ ", "|_ ", " _|"],
+        "6": [" _ ", "|_ ", "|_|"],
+        "7": [" _ ", "  |", "  |"],
+        "8": [" _ ", "|_|", "|_|"],
+        "9": [" _ ", "|_|", "  |"],
+        "0": [" _ ", "| |", "|_|"]
+    }
+
     let layerOne = "";
     let layerTwo = "";
     let layerThree = "";
     let strNumber = num.toString();
     for (let i = 0; i < strNumber.length; i++) {
-        layerOne += textToNumberLayerOne(num[i]);
-        layerTwo += textToNumberLayerTwo(num[i]);
-        layerThree += textToNumberLayerThree(num[i]);
+        layerOne += dict[strNumber[i]][0];
+        layerTwo += dict[strNumber[i]][1];
+        layerThree += dict[strNumber[i]][2];
     }
     return layerOne + '\n' + layerTwo + '\n' + layerThree;
 }
